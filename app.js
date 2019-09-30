130aeb7ef3f6c417ae18f42b8601c3789095b85c
Ilo <c_ilo@ats-hr420-dm40.local>
2019-09-30 18:16:50 -0400

    making changes with hbs and node

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path'); // path lets us navigate file system / folders
+const hbs = require('hbs');
 
 // heroku assigns a port wheni it deploys via the process (environment variables - comin)
 // locally this will run @ port 3000; remotely it'll run wherever heroku tells it to run
@@ -9,16 +10,26 @@ const app = express();
 
 app.use(express.static('public'));
 
+// tell express to use the handlebars engine to render data
+app.set('view engine', 'hbs');
+
+// tell express to use the views folder to find its templates
+app.set('views', __dirname + '/views');
+
 app.get('/', (req, res) => {
   console.log('at the home route');
-  res.sendFile(path.join(__dirname + '/views/index.html'));
+  // res.sendFile(path.join(__dirname + '/views/index.html'));
+
+  res.render('home', {message: "hi there!", anothermessage: "This is easy"} );
   // this builds localhost:3000/views/index.html
 })
 
 app.get('/contact', (req, res) => {
   console.log('at the contact route');
-  res.sendFile(path.join(__dirname + '/views/contact.html'));
+  //res.sendFile(path.join(__dirname + '/views/contact.html'));
   // this builds localhost:3000/views/contact.html
+
+  res.render('contact', {message: "hello"} );
 })
 
 app.get('/portfolio', (req, res) => {
